b7ea307b33d53854ad1f3511e0933ff6f7e842a4
Kamma Venkata Siva Rama Krishna <sivaramakrishna456123@gmail.com>
2025-11-16 16:01:54 +0530

    Added Window Init and Close in main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "raylib.h"
+
+int main()
+{
+    int window_width = 750;
+    int window_height = 700;
+
+    InitWindow(window_width,window_height,"Space Inavdersssuu");
+
+    CloseWindow();
+}
