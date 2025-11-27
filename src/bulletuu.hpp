f24279a4dceacc46bb1b2269cacf7dfd63524c42
Sai Kokkula <saikokkula20@gmail.com>
2025-11-27 14:13:02 +0530

    Enhance bulletuu class with comments
    Added comments to clarify member variables and methods.

--- src/bulletuu.hpp
+++ src/bulletuu.hpp
@@ -3,12 +3,12 @@
 
 class bulletuu{
     private:
-        Vector2 position;
-        int speed;
+        Vector2 position;//to determine where is the bullet beam on the screen 
+        int speed;//to determine how fast it moves on y axis
     public:
-        bulletuu(Vector2 position,int speed);
-        void update();
-        void draw();
+        bulletuu(Vector2 position,int speed);//constructor for setting position and speed 
+        void update();//update the postion of the laser beam
+        void draw();//to draw the bullet on the screen
         Rectangle getRectangle();
         
         bool active;    // if bullet is inside the window or not..
