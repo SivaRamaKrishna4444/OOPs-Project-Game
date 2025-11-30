018d3c7c5cfc85509a472c71e6accc0fcebcc661
Bharath <kbharathsai18@gmail.com>
2025-11-30 12:52:47 +0530

    Update obstacle.hpp

--- src/obstacle.hpp
+++ src/obstacle.hpp
@@ -6,9 +6,9 @@ class Obstacle{
     private:
 
     public:
-        Obstacle(Vector2 position);
-        void Draw();
+        Obstacle(Vector2 position); //builds an obstacle at a given position
+        void Draw(); //draw function to dsiplay all blocks of obstacles
         Vector2 position;
-        std :: vector<Block> blocks;
-        static std :: vector<std::vector<int>> grid;
+        std :: vector<Block> blocks; //store all small block pieces that makesup the obstacle
+        static std :: vector<std::vector<int>> grid;//2d grid pattern for generating sield shape
 };
