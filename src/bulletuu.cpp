7cdb0c0b96c3641a6954caa0fe4e8a79b35461f0
Bharath <kbharathsai18@gmail.com>
2025-11-30 12:14:05 +0530

    Update bulletuu.cpp

--- src/bulletuu.cpp
+++ src/bulletuu.cpp
@@ -1,28 +1,28 @@
 #include "bulletuu.hpp"
 
-bulletuu::bulletuu(Vector2 position, int speed)
+bulletuu::bulletuu(Vector2 position, int speed) //constructor to intitialize position and speed
 {
     this->position = position;//accesing the objects
     this->speed = speed;//accessing the objects 
-    active = true;
+    active = true; //bullet is active when created
 }
 
 void bulletuu::update()
 {
-    position.y = position.y + speed;
-    if(active)
+    position.y = position.y + speed; //move the bullet vertically
+    if(active) //check if the bullet is inside screen area or not
     {
-        if(position.y > GetScreenHeight() - 100 || position.y < 25)     //if it goes outside of the window or top of window..
+        if(position.y > GetScreenHeight() - 100 || position.y < 25)     //if it goes outside of the window or top of window. deactivate it so we cannot draw it anymore
         {
-            active = false;
+            active = false; //bullet will be disappeared
         }
     }
 }   
 
 void bulletuu::draw()
 {
-    if(active){
-        DrawRectangle(position.x,position.y,4,15,{255, 0, 255, 255});   //it will give bullet(as rectangle...)
+    if(active){ //only draw the bullet if it is active
+        DrawRectangle(position.x,position.y,4,15,{255, 0, 255, 255});   //it will give bullet(as rectangle shape...)
     }
 }
 
@@ -30,10 +30,10 @@ Rectangle bulletuu::getRectangle()
 {
     Rectangle rect;
 
-    rect.x = position.x;
-    rect.y = position.y;
-    rect.width= 4;
-    rect.height = 15;
+    rect.x = position.x; //rectangle position x
+    rect.y = position.y; //rectangle position y
+    rect.width= 4; //bullet width
+    rect.height = 15; //bullet height
 
     return rect;
 }
