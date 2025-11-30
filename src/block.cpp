3834bbbea52529c2d9849ca5430fd49ea96909a4
Navadeep54 <tanukulanavadeep@gmail.com>
2025-11-30 17:28:47 +0530

    Update block.cpp
    ntg

--- src/block.cpp
+++ src/block.cpp
@@ -1,21 +1,21 @@
 #include "block.hpp"
 
-Block::Block(Vector2 position)
+Block::Block(Vector2 position) //constructor to set the  starting position of block
 {
     this-> position = position;
 }
 
-void Block :: Draw(){
-    DrawRectangle(position.x,position.y,3,3,{243,216,63,255});
+void Block :: Draw(){ //function to draw the block onn the screen
+    DrawRectangle(position.x,position.y,3,3,{243,216,63,255}); //drawing rectangle with 3x3 size with a yelloish color
 }
 
-Rectangle Block::getRectangle()
+Rectangle Block::getRectangle() //function to return the collision rectangle of block
 {
     Rectangle rect;
-    rect.x = position.x;
-    rect.y = position.y;
-    rect.width = 3;
-    rect.height = 5;
+    rect.x = position.x; //x position of block
+    rect.y = position.y; //y postion of block
+    rect.width = 3; //width of block
+    rect.height = 5; //height of the block
     
     return rect;
 }
